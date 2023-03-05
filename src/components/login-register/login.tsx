3e4b8a01da23a7882014bd46bc677a4dedad3a80
Mustafa Kendigüzel <mustafa.kendiguzel@bil.omu.edu.tr>
2023-03-05 14:30:05 +0300

    change inputs color

--- src/components/login-register/login.tsx
+++ src/components/login-register/login.tsx
@@ -23,11 +23,21 @@ export class Login extends React.Component {
           <div className="form">
             <div className="form-group">
               <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
+              <input
+                className="text-[#000000]"
+                type="text"
+                name="username"
+                placeholder="username"
+              />
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" placeholder="password" />
+              <input
+                className="text-[#000000]"
+                type="password"
+                name="password"
+                placeholder="password"
+              />
             </div>
           </div>
         </div>
