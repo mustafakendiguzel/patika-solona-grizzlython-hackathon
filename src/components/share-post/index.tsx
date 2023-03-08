33e8e1a8daddeabb2521dfe8c2d7028a290edfff
Mustafa Kendigüzel <mustafa.kendiguzel@bil.omu.edu.tr>
2023-03-08 14:44:00 +0300

    update upload image part

--- src/components/share-post/index.tsx
+++ src/components/share-post/index.tsx
@@ -7,10 +7,11 @@ export const PostList: FC = () => {
   useEffect(() => {}, []);
   return (
     <div className="flex flex-col base-containers pt-5">
-      <div className="genelGrid grid grid-cols-4 gap-y-10 gap-x-10">
-        <div className="px-5 py-5 card mx-10 my-10 flex flex-col border-solid border-2 border-slate-400d">
+      <div className="genelGrid">
+        <div className="px-5 py-5 card mx-10 flex flex-col border-solid border-2 border-slate-400d">
           <div className="name self-center pb-5 underline decoration-[#a742e0] decoration-2 underline-offset-4 font-bold"></div>
-          <div>
+          <div className="h-80 w-80"></div>
+          {/* <div>
             {profilePicture && (
               <Image
                 className="rounded-full"
@@ -20,12 +21,14 @@ export const PostList: FC = () => {
                 height="300"
               />
             )}
-          </div>
+          </div> */}
           <div className="flex justify-center pt-3">
             <button
               onClick={() => {}}
-              className="followButton mt-2 px-5 rounded-md font-medium "
-            ></button>
+              className="followButton mt-2 px-5 rounded-md font-medium w-full "
+            >
+              SD
+            </button>
           </div>
         </div>
       </div>
