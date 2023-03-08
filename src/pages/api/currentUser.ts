fd268f339f5338d5ad17fb898cc3632bb1a9fde4
Mustafa Kendigüzel <mustafa.kendiguzel@bil.omu.edu.tr>
2023-03-08 03:01:36 +0300

    update current User

--- src/pages/api/currentUser.ts
+++ src/pages/api/currentUser.ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
+import clientPromise from "lib/mongodb";
+import { ObjectId } from "mongodb";
 
 type Data = {
   name: string;
@@ -12,10 +14,15 @@ export default async function handler(
 ) {
   const bearerHeader = req.headers["authorization"];
   const jwtKey = process.env.JWT_KEY;
+  const client = await clientPromise;
+  const db = client.db("dApp");
   if (typeof bearerHeader !== "undefined") {
     const bearer = bearerHeader.split(" ");
     const bearerToken = bearer[1];
     var payload;
+    const user = await db.collection("users").findOne({
+      _id: new ObjectId(payload.id as string),
+    });
     try {
       payload = await jwt.verify(bearerToken, jwtKey);
     } catch (e) {
@@ -24,7 +31,8 @@ export default async function handler(
       }
       return res.status(400).end();
     }
-    return res.status(200).json(payload);
+
+    return res.status(200).json(user);
   }
   return res.status(400).json({ message: "Bearer is undefined" });
 }
